a119e6ceace7eecbe04919c1ae868840499f472a
LeNPaul <l.nguyen.paul@gmail.com>
2017-04-09 22:35:49 -0400

    Add data point color based on density of data points

--- nfl-players/nfl_analysis.py
+++ nfl-players/nfl_analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import csv
 import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
 
 f = open('nfl_data.csv')
 csv_f = csv.reader(f)
@@ -17,8 +18,8 @@ next(csv_f)
 x_data = []
 y_data = []
 point_size = 20
-x_value = 9
-y_value = 10
+x_value = 9 #Height: 4
+y_value = 10 #Weight:5, Vert jump: 9, Broad jump: 10
 
 class_colours = ['slategrey', 'crimson', 'mediumseagreen', 'dodgerblue', 'salmon', 'darkorchid', 'sienna', 'orangered']
 
@@ -29,11 +30,15 @@ for row in csv_f:
         x_data.append(float(row[x_value]))
         y_data.append(float(row[y_value]))
 
-plt.scatter(x_data, y_data, marker='o', s=point_size,c='dodgerblue', edgecolor='none')
+# Calculate the point density
+xy = np.vstack([x_data,y_data])
+z = gaussian_kde(xy)(xy)
 
-#plt.title('')
-#plt.xlabel('')
-#plt.ylabel('')
+plt.scatter(x_data, y_data, marker='o', s=point_size,c=z, edgecolor='none')
+
+plt.title('Vertical Jump vs. Broad Jump of NFL Athletes')
+plt.xlabel('Vertical Jump (in)')
+plt.ylabel('Broad Jump (in)')
 
 #coefficients = np.polyfit(x_data, y_data, 1) #before: 3
 #polynomial = np.poly1d(coefficients)
